08be82caaf150585427f5cde1811c7c829e90274
Aligary <aligaryjr@gmail.com>
2022-06-18 00:49:19 -0700

    updated css

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -3,16 +3,6 @@ import "./Navbar.css"
 import Logo from "../Logo/Logo"
 
 export default function Navbar() {
-
-  // const productsByCategory =
-  //   Boolean(activeCategory) && activeCategory.toLowerCase() !== "all categories"
-  //     ? products.filter((p) => p.category === activeCategory.toLowerCase())
-  //     : products
-
-  // const productsToShow = Boolean(searchInputValue)
-  //   ? productsByCategory.filter((p) => p.name.toLowerCase().indexOf(searchInputValue))
-  //   : productsByCategory
-    
   return (
     <nav className="navbar">
         <Logo />
