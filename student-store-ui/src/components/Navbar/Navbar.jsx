c4ae1e687f75fa4ead05477327be634b9e5e9a65
Aligary <aligaryjr@gmail.com>
2022-06-17 20:45:47 -0700

    filter functions

--- student-store-ui/src/components/Navbar/Navbar.jsx
+++ student-store-ui/src/components/Navbar/Navbar.jsx
@@ -3,6 +3,16 @@ import "./Navbar.css"
 import Logo from "../Logo/Logo"
 
 export default function Navbar() {
+
+  // const productsByCategory =
+  //   Boolean(activeCategory) && activeCategory.toLowerCase() !== "all categories"
+  //     ? products.filter((p) => p.category === activeCategory.toLowerCase())
+  //     : products
+
+  // const productsToShow = Boolean(searchInputValue)
+  //   ? productsByCategory.filter((p) => p.name.toLowerCase().indexOf(searchInputValue))
+  //   : productsByCategory
+    
   return (
     <nav className="navbar">
         <Logo />
