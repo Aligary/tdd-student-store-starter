3cfb90e8a88d1d99dc374f0eb005d45ff9ca76f3
Aligary <aligaryjr@gmail.com>
2022-06-21 14:26:23 -0700

    search bar does not disappear

--- student-store-ui/src/components/SearchBar/SearchBar.jsx
+++ student-store-ui/src/components/SearchBar/SearchBar.jsx
@@ -5,36 +5,21 @@ import "./SearchBar.css"
 export default function SearchBar(props) {
 
   let activeSearch = false;
-  
-  
-
-  if(props.search != '')
-  {
     console.log(2, props.search)
     activeSearch = true;
-    return <ProductGrid
-      activeSearch={activeSearch}
-      search={props.search}
-      products={props.products}
-      handleAddItemToCart={props.handleAddItemToCart}
-      handleRemoveItemFromCart={props.handleRemoveItemFromCart}
-    />
-  }
-  else
-    {
-      
-      <ProductGrid
-      products={props.products}
-      handleAddItemToCart={props.handleAddItemToCart}
-      handleRemoveItemFromCart={props.handleRemoveItemFromCart}
-    />
-    }
-
-  return (
-    <div className="search-bar">
-      <div className="input">
-        <textarea type="text" name="search" placeholder="Search" value={props.search} onChange={props.handleOnChange}/>
+    return (
+        <div className="search-bar">
+        <div className="input">
+          <textarea type="text" name="search" placeholder="Search" value={props.search} onChange={props.handleOnChange}/>
+        </div>
+        <div>
+        <ProductGrid
+        activeSearch={activeSearch}
+        search={props.search}
+        products={props.products}
+        handleAddItemToCart={props.handleAddItemToCart}
+        handleRemoveItemFromCart={props.handleRemoveItemFromCart}
+      />
       </div>
     </div>
-  )
-}
+    )}
